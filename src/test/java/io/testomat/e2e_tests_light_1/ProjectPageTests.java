16693b54c8f50ff56b28197ad0f4e2f312048fd9
klym.myrvoda <mirvoda777>
2025-02-19 23:05:37 +0200

    hide credentials

--- src/test/java/io/testomat/e2e_tests_light_1/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light_1/ProjectPageTests.java
@@ -1,6 +1,7 @@
 package io.testomat.e2e_tests_light_1;
 
 import com.codeborne.selenide.Condition;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -9,25 +10,51 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class ProjectPageTests {
 
+    Dotenv dotenv = Dotenv.load();
+
+    String baseUrl = dotenv.get("BASE_URL");
+    String username = dotenv.get("USEREMAIL");
+    String password = dotenv.get("PASSWORD");
+    String projectName = "manufacture light";
+
     @Test
     public void userCanFindAndOpenProjectWithTests() {
-        open("https://app.testomat.io/");
 
-        // login
-        $("#content-desktop #user_email").setValue("klimmir@gmail.com");
-        $("#content-desktop #user_password").setValue("KKZ8Z!h34c5DihK");
+        login(baseUrl, username, password);
+
+        searchProject(projectName);
+
+        openProjectPage(projectName);
+
+        // logout
+        //...
+
+    }
+
+    @Test
+    public void test2() {
+
+        login(baseUrl, username, password);
+
+    }
+
+    private static void openProjectPage(String projectName) {
+        $(byText(projectName)).click();
+        $(".first h2").shouldHave(Condition.text(projectName));
+        $(".first [href*='/readme']").shouldHave(Condition.text("Readme"));
+    }
+
+    private static void searchProject(String projectName) {
+        $("#search").setValue(projectName);
+    }
+
+    private static void login(String url, String username, String password) {
+        open(url);
+        $("#content-desktop #user_email").setValue(username);
+        $("#content-desktop #user_password").setValue(password);
         $("#content-desktop #user_remember_me").click();
         $("#content-desktop [name='commit']").click();
         $(".common-flash-success-right").shouldBe(Condition.visible);
-
-        // search the test project
-        $("#search").setValue("manufacture light");
-
-        // open the project page
-        $(byText("manufacture light")).click();
-        $(".first h2").shouldHave(Condition.text("manufacture light"));
-
-
     }
 
 }
